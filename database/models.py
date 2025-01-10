9f1c7a6f37f2861eabec46b5eceda209228a35f5
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-01-11 01:56:04 +0300

    Fix generate timetables

--- database/models.py
+++ database/models.py
@@ -2,6 +2,9 @@ import datetime
 import peewee
 from config_data.config import BASE_DIR
 import os
+import threading
+from logger import app_logger
+from time import sleep
 
 db = peewee.SqliteDatabase(os.path.join(BASE_DIR, "database/database.db"))
 
@@ -37,14 +40,44 @@ def create_models():
 def create_time_tables():
     """ Функция для генерирования графика расписания консультаций на неделю. """
     cur_datetime = datetime.datetime.now()
-    for i in range(6):
+    for i in range(21):
         cur_date = cur_datetime + datetime.timedelta(days=i)
         if cur_date.weekday() not in [5, 6]:  # Будни, кроме выходных
-            for start_time in ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00"]:
-                end_time = start_time.split(":")[0] + ":59"
-                Timetable.create(date=cur_date, start_time=start_time, end_time=end_time)
+            if cur_date.weekday() in (0, 1, 2):
+                for start_time in ["10:00", "10:20", "10:40", "11:00", "11:20", "11:40",
+                                   "14:00", "14:20", "14:40", "15:00", "15:20", "15:40",
+                                   "16:00", "16:20", "16:40", "17:00", "17:20", "17:40"]:
+                    end_time = start_time.split(":")[0] + ":" + str(int(start_time.split(":")[1]) + 19)
+                    # Проверка, нет ли уже существующей записи
+                    if not Timetable.select().where(Timetable.date == cur_date,
+                                                    Timetable.start_time == start_time,
+                                                    Timetable.end_time == end_time).exists():
+                        Timetable.create(date=cur_date, start_time=start_time, end_time=end_time)
+            elif cur_date.weekday() in (3, 4):
+                for start_time in ["13:00", "13:20", "13:40", "14:00", "14:20", "14:40",
+                                   "15:00", "15:20", "15:40", "16:00", "16:20", "16:40"
+                                   "17:00", "17:20", "17:40", "18:00", "18:20", "18:40"]:
+                    end_time = start_time.split(":")[0] + ":" + str(int(start_time.split(":")[1]) + 19)
+                    if not Timetable.select().where(Timetable.date == cur_date,
+                                                    Timetable.start_time == start_time,
+                                                    Timetable.end_time == end_time).exists():
+                        Timetable.create(date=cur_date, start_time=start_time, end_time=end_time)
 
 def delete_time_tables():
     """ Функция для удаления старых устаревших записей графика (вчерашних и ранее) """
     cur_datetime = datetime.datetime.now()
     Timetable.delete().where(Timetable.date < cur_datetime.date()).execute()
+
+def generate_time_tables():
+    """ Функция для асинхронного запуска функции create_time_tables каждые 3 недели """
+    while True:
+        app_logger.info("Генерация графика расписания консультаций и удаление старых записей...")
+        threading.Timer(3 * 7 * 24 * 60 * 60, delete_time_tables).start()
+        threading.Timer(3 * 7 * 24 * 60 * 60, create_time_tables).start()
+        sleep(3 * 7 * 24 * 60 * 60)
+
+def start_generate():
+    """ Запуск генерации графика расписания консультаций в фоновом режиме """
+    delete_time_tables()
+    create_time_tables()
+    threading.Thread(target=generate_time_tables).start()
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from server_manager import server_1
-from database.models import create_models, create_time_tables, delete_time_tables
-import schedule
+from database.models import create_models, start_generate
 
 
 
@@ -10,9 +9,5 @@ def main():
 
 if __name__ == '__main__':
     create_models()  # Создаем таблицы в БД
-    # create_time_tables()
-
-    schedule.every().monday.at("00:01").do(delete_time_tables)
-    schedule.every().monday.at("00:01").do(create_time_tables)
-
+    start_generate()
     main()
