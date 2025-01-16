de86bd1b48f78fa4dd96b4949ca4b9372bf3a1e2
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-01-16 14:22:19 +0300

    Fix generate timetable

--- database/models.py
+++ database/models.py
@@ -55,7 +55,7 @@ def create_time_tables():
                         Timetable.create(date=cur_date, start_time=start_time, end_time=end_time)
             elif cur_date.weekday() in (3, 4):
                 for start_time in ["13:00", "13:20", "13:40", "14:00", "14:20", "14:40",
-                                   "15:00", "15:20", "15:40", "16:00", "16:20", "16:40"
+                                   "15:00", "15:20", "15:40", "16:00", "16:20", "16:40",
                                    "17:00", "17:20", "17:40", "18:00", "18:20", "18:40"]:
                     end_time = start_time.split(":")[0] + ":" + str(int(start_time.split(":")[1]) + 19)
                     if not Timetable.select().where(Timetable.date == cur_date,
