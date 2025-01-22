1ed46e9cd1faf98ee35400711d1d78c2d683d027
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-01-22 16:09:27 +0300

    Added the ability to cancel a record

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -5,6 +5,7 @@ from logger import app_logger
 from config_data.config import BASE_DIR, ADMIN_ID, KEYBOARD
 import os
 import datetime
+import json
 
 
 start_text = """
@@ -14,6 +15,28 @@ start_text = """
 
 ВАЖНО! Для записи на консультацию, вам должно быть не менее 21 года.
 """
+
+def get_current_timetable_from_timetable_str(cur_user: User, timetable_str: str) -> Timetable:
+    """
+    Функция для извлечения данных из строкового представления объекта Timetable
+    :param cur_user: объект пользователя
+    :param timetable_str: строковое представление
+    :return: Timetable объект
+    """
+    t_day, t_month = timetable_str.split(":")[0].split(".")
+    t_date = datetime.date(year=datetime.date.today().year, month=int(t_month), day=int(t_day))
+    t_hour_start, t_minute_start = timetable_str.split(":")[1][1:], timetable_str.split(":")[2].split(" ")[0]
+    t_hour_end, t_minute_end = timetable_str.split(":")[2].split(" - ")[1], timetable_str.split(":")[3]
+    t_time_start = datetime.time(int(t_hour_start), int(t_minute_start), 0)
+    t_time_end = datetime.time(int(t_hour_end), int(t_minute_end), 0)
+    # Получение объекта Timetable по данному дню, месяцу, времени начала и окончания
+    for timetable_obj in Timetable.select().where(Timetable.user_id == cur_user, Timetable.is_booked == True):
+        if (timetable_obj.date.day == t_date.day and timetable_obj.date.month == t_date.month
+        and timetable_obj.start_time.hour == t_time_start.hour and
+                timetable_obj.start_time.minute == t_time_start.minute
+        and timetable_obj.end_time.hour == t_time_end.hour and timetable_obj.end_time.minute == t_time_end.minute):
+            return timetable_obj
+    return None
 
 
 def start_handler(event: VkBotEvent, vk_api_elem) -> None:
@@ -158,6 +181,113 @@ def add_phone_handler(event: VkBotEvent, vk_api_elem, phone_number) -> None:
                               random_id=get_random_id(),
                               keyboard=KEYBOARD)
 keyboard = open(os.path.join(BASE_DIR, "keyboards/default.json"), "r", encoding="UTF-8").read()
+
+def my_timetables_handler(event: VkBotEvent, vk_api_elem) -> None:
+    """
+    Хендлер для отображения информации по текущим записям пользователя.
+    Бот выдает список текущих записей пользователя в виде кнопок.
+    Нажав на запись, можно просмотреть - под сообщением будет кнопка удалить.
+    :param event: VkBotEvent
+    :param vk_api_elem: VkApiMethod
+    :return: None
+    """
+    user_id = event.object.message["from_id"]
+    user: User = User.get_or_none(User.user_id == user_id)
+
+    if not user:
+        app_logger.warning(f"Пользователь с id {user_id} не найден в базе данных.")
+        return
+
+    # Получаем все записи Timetable пользователя
+    timetables = Timetable.select().where(Timetable.user_id == user, Timetable.is_booked == True)
+
+    if timetables.count() == 0:
+        app_logger.info(f"Пользователь {user.full_name} не имеет текущих записей.")
+        vk_api_elem.messages.send(peer_id=user_id,
+                                  message="У вас нет текущих записей на консультации.",
+                                  random_id=get_random_id(),
+                                  keyboard=KEYBOARD)
+        return
+
+    # Формируем текст сообщения с кнопками удаления для каждой записи
+    timetables_keyboard = {
+            "inline": True,
+            "buttons": []
+        }
+
+    timetables_str = [f"{elem.date.strftime("%d.%m")}: {elem.start_time.strftime("%H:%M")} - {elem.end_time.strftime("%H:%M")}"
+                      for elem in timetables]
+    for i, time_str in enumerate(timetables_str):
+        if i % 2 == 0:
+            timetables_keyboard["buttons"].append([])
+        timetables_keyboard["buttons"][i // 2].append({
+            "action": {
+                "type": "text",
+                "label": f"{str(time_str)}"
+            },
+            "color": "primary"
+        })
+    timetables_keyboard = json.dumps(timetables_keyboard)
+    app_logger.info(f"Пользователь {user.full_name} запросил информацию о своих записях")
+    vk_api_elem.messages.send(peer_id=user_id,
+                              message="Ваши текущие записи на консультации:",
+                              random_id=get_random_id(),
+                              keyboard=timetables_keyboard)
+
+def get_timetable_handler(event: VkBotEvent, vk_api_elem, timetable_str: str) -> None:
+    """
+     Хендлер для отображения записи на прием "
+    :param event: VkBotEvent
+    :param vk_api_elem: VkApiMethod
+    :param timetable_str: информация о приеме вида %m.%d: %H:%M - %H:%M
+    :return: None
+    """
+    user_id = event.object.message["from_id"]
+    cur_user: User = User.get_or_none(User.user_id == user_id)
+
+    cur_timetable_obj = get_current_timetable_from_timetable_str(cur_user, timetable_str)
+    app_logger.info(f"Пользователь {cur_user.full_name} получил информацию о записи {timetable_str}")
+    delete_keyboard = json.dumps({
+        "inline": True,
+        "buttons": [
+            [
+                {
+                    "action": {
+                        "type": "text",
+                        "payload": "{\"button\": \"1\"}",
+                        "label": f"Отменить запись {timetable_str}"
+                    },
+                    "color": "negative"
+                }
+            ],
+    ]})
+    vk_api_elem.messages.send(peer_id=user_id,
+                              message=f"Информация о записи:\n"
+                                      f"Дата: {cur_timetable_obj.date.strftime('%d.%m')}\n"
+                                      f"Время: c {cur_timetable_obj.start_time.strftime('%H:%M')} до "
+                                      f"{cur_timetable_obj.end_time.strftime('%H:%M')}",
+                              random_id=get_random_id(),
+                              keyboard=delete_keyboard)
+
+
+def delete_timetable_handler(event: VkBotEvent, vk_api_elem, timetable_str: str) -> None:
+    """
+    Хендлер удаления записи о приеме.
+    :param timetable_str: Информация о записи
+    :param event: VkBotEvent
+    :param vk_api_elem: VkApiMethod
+    :return: None
+    """
+    user_id = event.object.message["from_id"]
+    cur_user: User = User.get_or_none(User.user_id == user_id)
+    current_timetable_str = timetable_str.split("Отменить запись ")[1]
+    cur_timetable_obj = get_current_timetable_from_timetable_str(cur_user, current_timetable_str)
+    cur_timetable_obj.delete_instance()
+    app_logger.info(f"Пользователь {cur_user.full_name} отменил запись {current_timetable_str}")
+    vk_api_elem.messages.send(peer_id=user_id,
+                              message=f"Запись {current_timetable_str} отменена!",
+                              random_id=get_random_id(),
+                              keyboard=KEYBOARD)
 
 def get_report_handler(event: VkBotEvent, vk_api_elem) -> None:
     """
